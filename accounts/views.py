0aaf7db6f951b794b2178069a64387f48373e2ff
Erajbek Ahmadjonov <axmadjonoverajbek@gmail.com>
2025-11-17 13:26:23 +0500

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CustomRegisterForm
 
-def home(request):
-    return HttpResponse("Salom")
+def register(request):
+    if request.method == "POST":
+        form = CustomRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = CustomRegisterForm()
+    return render(request, "accounts/register.html", {"form": form})
 
--- form_auth/urls.py
+++ form_auth/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import home
+from accounts.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/register/', register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', home),  
 ]
