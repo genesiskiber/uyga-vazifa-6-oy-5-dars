49c2408f328968d8596cf5048d9bd67944e2612d
Erajbek Ahmadjonov <axmadjonoverajbek@gmail.com>
2025-11-17 13:10:04 +0500

    Initial commit

---
+++ simple_auth/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from accounts.views import home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', home),  
+]
